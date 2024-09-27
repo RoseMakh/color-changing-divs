4d6addd3842b1a8fef9f806f1c196ab654c0340b
Rose Makh <42286128+RoseMakh@users.noreply.github.com>
2024-09-26 21:21:02 -0400

    Add files via upload
    Added: Div size changing function

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const cc = console.log;
+const cc = console.log; //for testing
 const container = document.getElementById("container");
 const resetBtn = document.getElementById("reset");
 const hslValueDisplay = document.getElementById("hslValue");
 
+let changeSize = "Large";
+let sizeValue = "";
 let speed = 0;
-let divsNum = 40;
+let startDiv = 1;
+let prevStartDiv = 0;
+let divsNum = 0;
+let prevDivsNum = 0;
 let divVars = {};
 
 let changeMode = "Hue";
@@ -16,26 +21,87 @@ let id;
 
 resetBtn.addEventListener("click", resetGrid);
 
-//ADDING DIVS INTO DIVVARS DYNAMICALLY
-for (let i = 1; i <= divsNum; i++) {
-  let divName = "div" + i;
-  let newDiv = document.createElement("DIV");
-  newDiv.setAttribute("id", divName);
-
-  container.append(newDiv);
-  let getNewDiv = document.getElementById(divName);
-
-  getNewDiv.addEventListener("mousemove", colorChange);
-
-  divVars[divName] = {
-    divHue: 340,
-    directionHue: 1,
-    divSaturation: 100,
-    directionSaturation: 1,
-    divLightness: 50,
-    directionLightness: 1,
-  };
+function changeDivsSize() {
+  for (const child of container.children) {
+    document.getElementById(child.id).style.width = sizeValue;
+    document.getElementById(child.id).style.height = sizeValue;
+  }
 }
+
+function removeDivs() {
+  cc("remove divs");
+  for (const e in divVars) {
+    if (Number(e.substring(3)) > Math.pow(divsNum, 2)) {
+      document.getElementById(e).remove();
+      delete divVars[e];
+    }
+  }
+  cc("divVars inside of remove");
+  cc(divVars);
+}
+
+function determineStartDiv() {}
+
+function determineNums() {
+  switch (changeSize) {
+    case "Small":
+      divsNum = 32;
+      sizeValue = 400 / divsNum + "px"; //12.5px
+      break;
+    case "Medium":
+      divsNum = 16;
+      sizeValue = 400 / divsNum + "px"; //25px
+      break;
+    case "Large":
+      divsNum = 8;
+      sizeValue = 400 / divsNum + "px"; //50px
+      break;
+  }
+
+  if (prevDivsNum !== 0) {
+    //larger divs, fewer divs
+    if (prevDivsNum > divsNum) {
+      removeDivs();
+    }
+
+    changeDivsSize();
+
+    //smaller divs, more divs
+    if (prevDivsNum < divsNum) {
+      addDivs(Math.pow(prevDivsNum, 2) + 1);
+    }
+  }
+}
+determineNums();
+
+//ADDING DIVS INTO DIVVARS AND DOM DYNAMICALLY
+function addDivs(divs) {
+  for (let i = divs; i <= Math.pow(divsNum, 2); i++) {
+    let divName = "div" + i;
+    let newDiv = document.createElement("DIV");
+    newDiv.setAttribute("id", divName);
+
+    container.append(newDiv);
+    let getNewDiv = document.getElementById(divName);
+
+    getNewDiv.style.width = sizeValue;
+    getNewDiv.style.height = sizeValue;
+    getNewDiv.addEventListener("mousemove", colorChange);
+
+    if (!divVars[divName]) {
+      divVars[divName] = {
+        divHue: 340,
+        directionHue: 1,
+        divSaturation: 100,
+        directionSaturation: 1,
+        divLightness: 50,
+        directionLightness: 1,
+        moused: false,
+      };
+    }
+  }
+}
+addDivs(1);
 
 function selectMode() {
   changeMode = this.value;
@@ -46,10 +112,24 @@ modeOptions.forEach((e) => {
   e.addEventListener("click", selectMode);
 });
 
+function selectSize() {
+  if (this.value !== changeSize) {
+    prevDivsNum = divsNum;
+    changeSize = this.value;
+    determineNums();
+  }
+}
+
+let sizeOptions = document.querySelectorAll("input[name = size]");
+sizeOptions.forEach((e) => {
+  e.addEventListener("click", selectSize);
+});
+
 function colorChange() {
   d = divVars[this.id];
   id = document.getElementById(this.id);
-
+  d.moused = true;
+  cc(this.id);
   hslValueDisplay.textContent = `Current Color: hsl( ${d.divHue}, ${d.divSaturation}, ${d.divLightness} )`;
 
   if (changeMode === "Lightness" || changeMode === "Saturation") {
@@ -73,15 +153,16 @@ function colorChange() {
 }
 
 function resetGrid() {
-  let x = divVars;
-  for (const e in x) {
-    x[e] = {
+  //let x = divVars;
+  for (const e in divVars) {
+    divVars[e] = {
       divHue: 340,
       directionHue: 1,
       divSaturation: 100,
       directionSaturation: 1,
       divLightness: 50,
       directionLightness: 1,
+      moused: false,
     };
   }
   for (const child of container.children) {
