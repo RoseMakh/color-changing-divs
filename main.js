fb066ee6186834acbc3ee14b8f5639c9a8e03d8d
Rose Makh <42286128+RoseMakh@users.noreply.github.com>
2024-09-26 23:27:37 -0400

    Add files via upload
    Cleaned up the CSS and JavaScript.  Added comments to the JavaScript.

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 const cc = console.log; //for testing
-const container = document.getElementById("container");
-const resetBtn = document.getElementById("reset");
-const hslValueDisplay = document.getElementById("hslValue");
+const container = document.getElementById("container"); //container that holds the divs
+const resetBtn = document.getElementById("reset"); //button that resets hsl in the object and resets all divs background color to white
+const hslValueDisplay = document.getElementById("hslValue"); //element that displays the HSL value of the current or last div
 
-let changeSize = "Large";
-let sizeValue = "";
-let speed = 0;
-let startDiv = 1;
-let prevStartDiv = 0;
-let divsNum = 0;
-let prevDivsNum = 0;
-let divVars = {};
+let changeSize = "Large"; //div size
+let sizeValue = ""; //current height/width for divs
+let speed = 0; //speed at which an HSL parameter changes
+let startDiv = 1; //number of the starting div for adding divs to the container
+let prevStartDiv = 0; //the previous value of startDiv
+let divsNum = 0; //number of divs per row or column
+let prevDivsNum = 0; //previous number or divs per row or column
+let divVars = {}; //object that holds all the div stats
 
+//Used to determine which HSL parameter to manipulate
+//Options: Hue , Saturation , Lightness
 let changeMode = "Hue";
-//Hue , Saturation , Lightness
 
-//For the colorChange function
+//Used inside the colorChange function only. Declared here so they aren't created anew on evey mouse move
 let d;
 let id;
 
@@ -28,20 +29,18 @@ function changeDivsSize() {
   }
 }
 
+//Remove divs from container and div objects from divVars
 function removeDivs() {
-  cc("remove divs");
   for (const e in divVars) {
     if (Number(e.substring(3)) > Math.pow(divsNum, 2)) {
       document.getElementById(e).remove();
       delete divVars[e];
     }
   }
-  cc("divVars inside of remove");
   cc(divVars);
 }
 
-function determineStartDiv() {}
-
+//Update the number of divs per column/row and the height/width of the divs
 function determineNums() {
   switch (changeSize) {
     case "Small":
@@ -59,14 +58,14 @@ function determineNums() {
   }
 
   if (prevDivsNum !== 0) {
-    //larger divs, fewer divs
+    //larger divs = fewer divs, so remove some before changing their sizes
     if (prevDivsNum > divsNum) {
       removeDivs();
     }
 
     changeDivsSize();
 
-    //smaller divs, more divs
+    //smaller divs = more divs, so add some to the container
     if (prevDivsNum < divsNum) {
       addDivs(Math.pow(prevDivsNum, 2) + 1);
     }
@@ -74,7 +73,8 @@ function determineNums() {
 }
 determineNums();
 
-//ADDING DIVS INTO DIVVARS AND DOM DYNAMICALLY
+//Add divs into the container element and the divVars object
+//startDiv is always passed in as the argument
 function addDivs(divs) {
   for (let i = divs; i <= Math.pow(divsNum, 2); i++) {
     let divName = "div" + i;
@@ -88,6 +88,7 @@ function addDivs(divs) {
     getNewDiv.style.height = sizeValue;
     getNewDiv.addEventListener("mousemove", colorChange);
 
+    //If the div's object isn't in divVars, add it
     if (!divVars[divName]) {
       divVars[divName] = {
         divHue: 340,
@@ -107,11 +108,13 @@ function selectMode() {
   changeMode = this.value;
 }
 
+//Add event listener to the mode select radio inputs
 let modeOptions = document.querySelectorAll("input[name = mode]");
 modeOptions.forEach((e) => {
   e.addEventListener("click", selectMode);
 });
 
+//if user selects a new size, update variables before calculating the new values
 function selectSize() {
   if (this.value !== changeSize) {
     prevDivsNum = divsNum;
@@ -120,11 +123,13 @@ function selectSize() {
   }
 }
 
+//Add event listener to the size select radio inputs
 let sizeOptions = document.querySelectorAll("input[name = size]");
 sizeOptions.forEach((e) => {
   e.addEventListener("click", selectSize);
 });
 
+//Change the div colors
 function colorChange() {
   d = divVars[this.id];
   id = document.getElementById(this.id);
@@ -132,6 +137,7 @@ function colorChange() {
   cc(this.id);
   hslValueDisplay.textContent = `Current Color: hsl( ${d.divHue}, ${d.divSaturation}, ${d.divLightness} )`;
 
+  //Change some values depending on the current HSL parameter mode
   if (changeMode === "Lightness" || changeMode === "Saturation") {
     speed = 1;
     if (d["div" + changeMode] >= 100) {
@@ -148,12 +154,14 @@ function colorChange() {
       d["div" + changeMode] = 0;
     }
   }
+
+  //Update the HSL value display
   d["div" + changeMode] += speed * d["direction" + changeMode];
   id.style.backgroundColor = `hsl(${d.divHue} ${d.divSaturation} ${d.divLightness})`;
 }
 
+//Reset the divs, div objects, and HSL value display to their defaults
 function resetGrid() {
-  //let x = divVars;
   for (const e in divVars) {
     divVars[e] = {
       divHue: 340,
