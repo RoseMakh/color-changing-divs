5d7371b574470e6f01776fabe9194b8287f857ca
Rose Makh <42286128+RoseMakh@users.noreply.github.com>
2024-10-13 14:08:29 -0400

    Version J
    Added follow and color scheme buttons with animated drop button menus. Updated the list mode color list layout. Animated the mobile menu open/close button. Added instructions with an animated slide-in.

--- main.js
+++ main.js
@@ -10,13 +10,35 @@ const getListBtn = document.getElementById("listBtn");
 const getHslValue = document.getElementById("hslValue"); //element that displays the HSL value of the current or last div
 const getMobileMenuDiv = document.getElementById("mobileMenuDiv");
 const getMobileMenuBtn = document.getElementById("mobileMenuBtn");
+const getTopDash = document.getElementById("topDash");
+const getMiddleDash = document.getElementById("middleDash");
+const getBottomDash = document.getElementById("bottomDash");
 const getMobileMenuP = document.getElementById("mobileMenuP");
 const getDateP = document.getElementById("dateP");
 const getListModeContent = document.getElementById("listModeContent");
 const getListModeHeading = document.getElementById("listModeHeading");
 const getListModeSelections = document.getElementById("listModeSelections");
 const getHiddenPrintables = document.getElementById("hiddenPrintables");
+const getInstructionsHeading = document.getElementById("instructionsHeading");
+const getInstructionsBody = document.getElementById("instructionsBody");
 
+//TOP RIGHT CORNER BUTTONS
+//holds all the follow buttons
+const getFollowBtnDiv = document.getElementById("followBtnDiv");
+const getBtnInstagram = document.getElementById("btnInstagram");
+const getBtnFacebook = document.getElementById("btnFacebook");
+const getBtnTwitterX = document.getElementById("btnTwitterX");
+const getBtnFollow = document.getElementById("btnFollow");
+
+//holds all the color scheme buttons
+const getSchemeBtnDiv = document.getElementById("schemeBtnDiv");
+const getBtnSchemeSystem = document.getElementById("btnSchemeSystem");
+const getBtnSchemeDark = document.getElementById("btnSchemeDark");
+const getBtnSchemeLight = document.getElementById("btnSchemeLight");
+const getBtnScheme = document.getElementById("btnScheme");
+
+//
+////END OF CONSTANTS
 let divSize = "Large"; //div size
 let sizeValue = ""; //current height/width for divs
 let speed = 0; //speed at which an HSL parameter changes
@@ -28,7 +50,7 @@ let divContainerSize = Math.min(400, window.visualViewport.width * 0.9); //heigh
 let divVars = {}; //object that holds all the div stats
 let listModeDivArray = []; //holds the HSL values of selected divs in Div List Mode
 
-let innerOrListMousable = false;
+let innerMousable = false;
 
 let currentElem;
 
@@ -66,6 +88,81 @@ getDateP.textContent = `Printed on ${new Date().toLocaleDateString("en-us", {
   day: "numeric",
 })}`;
 
+//Animations for follow and theme menus
+
+function animateSmallFollowBtns() {
+  if (!getBtnInstagram.style.transitionDelay) {
+    getBtnInstagram.style.transitionDelay = "0s";
+  } else if (getBtnInstagram.style.transitionDelay === "0s") {
+    getBtnInstagram.style.transitionDelay = "0.4s";
+    getBtnTwitterX.style.transitionDelay = "0s";
+  } else {
+    getBtnInstagram.style.transitionDelay = "0s";
+    getBtnTwitterX.style.transitionDelay = "0.4s";
+  }
+  getBtnInstagram.classList.toggle("animSlideReveal1");
+  getBtnFacebook.classList.toggle("animSlideReveal2");
+  getBtnTwitterX.classList.toggle("animSlideReveal3");
+}
+
+function animateSmallSchemeBtns() {
+  if (!getBtnSchemeLight.style.transitionDelay) {
+    getBtnSchemeLight.style.transitionDelay = "0s";
+  } else if (getBtnSchemeLight.style.transitionDelay === "0s") {
+    getBtnSchemeLight.style.transitionDelay = "0.4s";
+    getBtnSchemeSystem.style.transitionDelay = "0s";
+  } else {
+    getBtnSchemeLight.style.transitionDelay = "0s";
+    getBtnSchemeSystem.style.transitionDelay = "0.4s";
+  }
+  getBtnSchemeLight.classList.toggle("animSlideReveal1");
+  getBtnSchemeDark.classList.toggle("animSlideReveal2");
+  getBtnSchemeSystem.classList.toggle("animSlideReveal3");
+}
+
+getBtnFollow.addEventListener("click", animateSmallFollowBtns);
+getBtnScheme.addEventListener("click", animateSmallSchemeBtns);
+
+function animateInstructionsBody() {
+  cc(this.id + " in animate instbody");
+  toggleClass("hiddenByMoveHorizontal", getInstructionsBody);
+  toggleClass("activeBtn", getInstructionsHeading);
+}
+getInstructionsHeading.addEventListener("click", animateInstructionsBody);
+
+//close all open menus if anything other than the menu is clicked
+window.addEventListener("click", function (e) {
+  if (
+    !getBtnFollow.contains(e.target) &&
+    getBtnInstagram.classList.contains("animSlideReveal1")
+  ) {
+    animateSmallFollowBtns();
+  } else if (
+    !getBtnScheme.contains(e.target) &&
+    getBtnSchemeLight.classList.contains("animSlideReveal1")
+  ) {
+    animateSmallSchemeBtns();
+  }
+
+  if (
+    !getInstructionsHeading.contains(e.target) &&
+    getInstructionsHeading.classList.contains("activeBtn")
+  ) {
+    animateInstructionsBody();
+  }
+
+  if (window.visualViewport.width < window.visualViewport.height) {
+    if (
+      !getMobileMenuDiv.contains(e.target) &&
+      !getMobileMenuBtn.contains(e.target) &&
+      !getMobileMenuDiv.classList.contains("hiddenByMove")
+    ) {
+      mobileMenuBtnAnimate();
+      toggleClass("hiddenByMove", getMobileMenuDiv);
+    }
+  }
+});
+
 //set the size of the color divs
 getDivContainer.style.height = divContainerSize + "px";
 getDivContainer.style.width = divContainerSize + "px";
@@ -80,20 +177,52 @@ function toggleClass(className, ...elements) {
 
 //change the layout if viewport orientation is vertical/portrait
 if (window.visualViewport.width < window.visualViewport.height) {
+  //for vertical screens
+
+  toggleClass("hiddenByDisplay", getMobileMenuBtn);
+
   function mobileMenuHideShow() {
     toggleClass("hiddenByMove", getMobileMenuDiv);
   }
 
-  getMain.style.height = window.visualViewport.height + "px";
-  getMain.style.overflow = "hidden"; //prevent scrolling
+  function mobileMenuBtnAnimate() {
+    if (
+      !getTopDash.classList.contains("animClassMobileBtnTopDashOpen") &&
+      !getTopDash.classList.contains("animClassMobileBtnTopDashClose")
+    ) {
+      getTopDash.classList.add("animClassMobileBtnTopDashOpen");
+      getMiddleDash.classList.add("animClassMobileBtnMiddleDashOpen");
+      getBottomDash.classList.add("animClassMobileBtnBottomDashOpen");
+    } else if (getTopDash.classList.contains("animClassMobileBtnTopDashOpen")) {
+      getTopDash.classList.remove("animClassMobileBtnTopDashOpen");
+      getTopDash.classList.add("animClassMobileBtnTopDashClose");
 
-  //create the button that shows and hides the mobile menu
-  let newBtn = document.createElement("BUTTON");
-  newBtn.setAttribute("id", "mobileMenuBtn");
-  getMain.append(newBtn);
-  document
-    .getElementById("mobileMenuBtn")
-    .addEventListener("click", mobileMenuHideShow);
+      getMiddleDash.classList.remove("animClassMobileBtnMiddleDashOpen");
+      getMiddleDash.classList.add("animClassMobileBtnMiddleDashClose");
+
+      getBottomDash.classList.remove("animClassMobileBtnBottomDashOpen");
+      getBottomDash.classList.add("animClassMobileBtnBottomDashClose");
+    } else {
+      getTopDash.classList.remove("animClassMobileBtnTopDashClose");
+      getTopDash.classList.add("animClassMobileBtnTopDashOpen");
+
+      getMiddleDash.classList.remove("animClassMobileBtnMiddleDashClose");
+      getMiddleDash.classList.add("animClassMobileBtnMiddleDashOpen");
+
+      getBottomDash.classList.remove("animClassMobileBtnBottomDashClose");
+      getBottomDash.classList.add("animClassMobileBtnBottomDashOpen");
+    }
+  }
+
+  getMain.style.height = window.visualViewport.height + "px";
+  // getMain.style.overflow = "hidden"; //prevent scrolling
+
+  getMobileMenuBtn.addEventListener("click", () => {
+    mobileMenuHideShow();
+    mobileMenuBtnAnimate();
+  });
+
+  //mobileMenuHideShow
 
   getMobileMenuDiv.classList.add("mobileMenuMobile", "hiddenByMove");
   toggleClass("hiddenByDisplay", getMobileMenuP);
@@ -204,7 +333,7 @@ sizeOptions.forEach((e) => {
 //Change the div colors
 function colorChange() {
   if (
-    (mouseoverMode === "inner" && innerOrListMousable != true) ||
+    (mouseoverMode === "inner" && innerMousable != true) ||
     mouseoverMode === "list"
   ) {
     return;
@@ -242,9 +371,7 @@ function colorChange() {
 //FUNCTIONS FOR INNER AND LIST MODES
 function divInnerMode() {
   if (mouseoverMode === "inner") {
-    innerOrListMousable === false
-      ? (innerOrListMousable = true)
-      : (innerOrListMousable = false);
+    innerMousable === false ? (innerMousable = true) : (innerMousable = false);
   }
 }
 
@@ -253,6 +380,13 @@ function divListModeEnter() {
     return;
   }
 
+  toggleClass("deactivatedBtn", getResetBtn);
+
+  getDivContainer.classList.add("noMouseoverModeMarker-gray");
+
+  // if (window.visualViewport.width < window.visualViewport.height) {
+  //   getMain.style.overflow = "auto";
+  // }
   function pushColor() {
     for (const e in divVars) {
       cc("in pushcolor " + e);
@@ -277,6 +411,8 @@ function divListModeEnter() {
     let makeIdListDiv;
     let makeIdListBtnHolder;
     let makeIdListDeleteBtn;
+    let deleteBtn =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>';
 
     //Remove duplicates from the HSL values array
     listModeDivArray = listModeDivArray.filter(
@@ -309,7 +445,7 @@ function divListModeEnter() {
       document
         .getElementById(makeIdListDeleteBtn)
         .classList.add("listModeLiBtn");
-      document.getElementById(makeIdListDeleteBtn).textContent = "X";
+      document.getElementById(makeIdListDeleteBtn).innerHTML = deleteBtn;
       document
         .getElementById(makeIdListDeleteBtn)
         .addEventListener("click", removeListModeItem);
@@ -327,6 +463,10 @@ function divListModeEnter() {
 }
 
 function divListModeExit() {
+  toggleClass("deactivatedBtn", getResetBtn);
+
+  getDivContainer.classList.remove("noMouseoverModeMarker-gray");
+
   //hide the hsl values list
   toggleClass("hiddenByDisplay", getListModeContent);
 
@@ -370,8 +510,8 @@ function ChangeMouseoverMode() {
   //correctly sets the variables for divInnerMode and divListMode
   if (this.id === "listBtn" || this.id === "innerBtn") {
     mouseoverMode !== thisBtnModeName
-      ? innerOrListMousable === false
-      : innerOrListMousable === true;
+      ? innerMousable === false
+      : innerMousable === true;
   }
 
   mouseoverMode === "mouse"
@@ -403,6 +543,10 @@ getPrintBtn.addEventListener("click", print.bind(window));
 
 //Reset the divs, div objects, and HSL value display to their defaults
 function resetGrid() {
+  if (mouseoverMode === "list") {
+    return;
+  }
+
   for (const e in divVars) {
     divVars[e] = { ...divVarsDivStatsDefault };
   }
